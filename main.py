52c8fc179b84f381e1b56dfc40e53fcac169473b
Rihler <ermakoffsergey@gmail.com>
2023-10-18 14:32:56 +0300

    crud 2.0 car

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ cars = {}
 
 
 class Car():
-    def __init__(self, vin, model = None, brand = None, year = None):
+    def __init__(self, vin, model=None, brand=None, year=None):
         self.vin = vin
         self.model = model
         self.brand = brand
@@ -25,13 +25,13 @@ class Car():
         return True
 
     def valid_car(self):
-        if len(self.vin) != 17 or "I" in self.self.vin or "O" in self.vin or "Q" in self.vin or not (self.valid_sym(self.vin)):
+        if len(self.vin) != 17 or "I" in self.vin or "O" in self.vin or "Q" in self.vin or not (self.valid_sym(self.vin)):
             self.errors["vin"] = "Vin код не должен содержать I, Q, O и спец. знаков\
-             и его длина должна быть 17 символов"
+и его длина должна быть 17 символов"
         if self.brand.isdigit() or not (self.brand[0].isalpha()) or not (self.valid_sym(self.brand)):
             self.errors["brand"] = "Марка не должна содержать спец.\
              символы и цифры и не должна начинаться с цифр "
-        if not(self.year.isdigit()) and not (1950 < int(self.year) <= 2023):
+        if not (self.year.isdigit()) and not (1950 < int(self.year) <= 2023):
             self.errors["year"] = "Год должен быть числом от 1950 до 2023 включительно"
         if not (self.valid_sym(self.model)):
             self.errors["vin"] = "Модель не должна содержать спец. символы"
@@ -43,9 +43,12 @@ class Car():
 def append_car(vin, brand, model, year):
     car = Car(vin, model, brand, year)
     car.valid_car()
+    flag = True
     for i in car.errors:
         if car.errors[i] != "valid":
-            return car.errors
+            flag = False
+    if not(flag):
+        return car.errors
     if str(vin) not in cars.keys():
         cars[str(vin)] = {"Model": model.lower(), "Brand": brand.lower(), "Year": year}
         return "202", {str(vin): cars[str(vin)]}
@@ -54,7 +57,6 @@ def append_car(vin, brand, model, year):
 
 @app.delete('/delete/{vin}')
 def delete_car(vin):
-    car = Car()
     if str(vin) in cars.keys():
         vin1 = vin
         del cars[str(vin)]
@@ -68,11 +70,12 @@ def read_data_car():
 
 
 @app.put("/update/car/{vin}/")
-def update_data_car(vin: int = Path(lt=10 ** 18, ge=10 * 17), model: str = Path(min_length=4, max_length=8),
-                    brand: str = Path(min_length=4, max_length=8), year: int = Path(lt=2023, ge=1950)):
-    if str(vin) in cars.keys():
-        if valid_brand(brand):
-            return "Brand doesnt include numbers"
-        cars[str(vin)] = {"Model": model, "Brand": brand, "Year": year}
-        return "202", {model: cars[str(vin)], "vin": vin}
+def update_data_car(vin, brand, model, year):
+    car = Car(vin, brand, model, year)
+    car.valid_car()
+    for i in car.errors:
+        if car.errors[i] != "valid":
+            return car.errors
+    if vin in cars.keys:
+        cars[str(vin)] = {"Model": model.lower(), "Brand": brand.lower(), "Year": year}
     return f"{vin} does not exist in the database"
