8d81520ef33b204fafe9a89a59a90b2f768472e5
Rihler <ermakoffsergey@gmail.com>
2023-10-18 11:21:28 +0300

    valid data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,19 +10,29 @@ order = {}
 cars = {}
 
 
+# CRUD для автомобилей
+def valid_brand(brand):
+    return any(map(str.isdigit, brand))
+
 @app.post('/car/{vin}/{brand}/{model}/{year}')
-def append_car(vin: int, model: str, brand: str, year: int):
-    cars[str(vin)] = {"Model": model, "Brand": brand, "Year": year}
-    return "202", {str(vin): cars[str(vin)]}
+def append_car(vin: int = Path(lt=10 ** 18, ge=10 * 17), model: str = Path(min_length=4, max_length=8),
+               brand: str = Path(min_length=4, max_length=8), year: int = Path(lt=2023, ge=1950)):
+    if valid_brand(brand):
+        return "Brand doesnt include numbers"
+    if str(vin) not in cars.keys():
+        cars[str(vin)] = {"Model": model.lower(), "Brand": brand.lower(), "Year": year}
+        return "202", {str(vin): cars[str(vin)]}
+    return f"{vin} already exists"
 
 
 @app.delete('/delete/{vin}')
-def delete_car(vin: int):
+def delete_car(vin: int = Path(lt=10 ** 18, ge=10 * 17)):
     if str(vin) in cars.keys():
         vin1 = vin
         del cars[str(vin)]
         return f"202, {vin1} is deleted"
     return f"{vin} does not exist in the database"
+
 
 @app.get('/read/cars/')
 def read_data_car():
@@ -29,8 +40,11 @@ def read_data_car():
 
 
 @app.put("/update/car/{vin}/")
-def update_data_car(vin: int, model: str, brand: str, year: int):
+def update_data_car(vin: int = Path(lt=10 ** 18, ge=10 * 17), model: str = Path(min_length=4, max_length=8),
+                    brand: str = Path(min_length=4, max_length=8), year: int = Path(lt=2023, ge=1950)):
     if str(vin) in cars.keys():
+        if valid_brand(brand):
+            return "Brand doesnt include numbers"
         cars[str(vin)] = {"Model": model, "Brand": brand, "Year": year}
         return "202", {model: cars[str(vin)], "vin": vin}
     return f"{vin} does not exist in the database"
